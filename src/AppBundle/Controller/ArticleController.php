d9f2e705e84308d3c255a978528992e954eea73d
HBoughid <hamidboughida@hotmail.fr>
2016-03-30 09:49:19 +0200

    Ajout ArticleController

---
+++ src/AppBundle/Controller/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HMD
+ * Date: 30/03/2016
+ * Time: 09:41
+ */
+
+namespace AppBundle\Controller\Article;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class ArticleController extends Controller
+{
+    public function listAction()
+    {
+        
+    }
+}
