ee78a71a9a5af7e5f13ad0b2a4cd4573bbf36733
HBoughid <hamidboughida@hotmail.fr>
2016-03-30 09:05:06 +0200

    Test et ajout annotation route

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,9 +3,14 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route()
+     */
+
     public function indexAction()
     {
         return $this->render('AppBundle:Default:index.html.twig');
