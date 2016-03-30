d17cff4b497e67b0ab54f89d557d1828e4feb690
HBoughid <hamidboughida@hotmail.fr>
2016-03-30 09:28:25 +0200

    Ajout return reponse

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -13,6 +14,6 @@ class DefaultController extends Controller
 
     public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+        return new Response("Hello World !");
     }
 }
